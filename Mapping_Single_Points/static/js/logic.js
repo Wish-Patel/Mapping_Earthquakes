9d16c25296daa5088eea87dcce273de140229cea
Wish Patel <patelnovi@gmail.com>
2020-01-11 19:13:27 -0800

    adding logic for mapping a single point

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,14 +2,16 @@
 //console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-        40.7, -94.5
-    ],
-    zoom: 4
-});
+let map = L.map("mapid").setView([34.0522, -118.2437],14);
 
-let title_url = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}';
+let title_url = 'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}';
+//regular view: 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}'
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer(title_url, {
